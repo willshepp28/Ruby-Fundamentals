ea4f4b3447d2ba0adb0758accf61a79cf4c5b730
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-12-09 16:44:29 -0800

    added string-interpoliation.strings.rb file

---
+++ Basic-Datatypes/strings/concatenation.strings.rb
@@ -0,0 +1,7 @@
+# Concatenation is a way of joining two or more strings together to create a new string
+puts "Hello" + " John"
+
+language = "Spanish"
+puts "I also speak " + language
+
+
---
+++ Basic-Datatypes/strings/string-interpolation.strings.rb
@@ -0,0 +1,22 @@
+# String interpolation lets you embed expressions in a string
+
+name = "bob"
+
+# Instead of using the + operater, you can use the #{} syntax
+puts "Hello #{name}"
+
+
+# Another example, using multiple strings
+saturday = "Saturday"
+friday = "Friday"
+
+puts "My favorite days of the week are #{friday} and #{saturday}";
+
+
+# String interploation can also convert different data types to strings
+john = "John"
+age = 18
+
+puts "#{john} is #{age}"
+
+
