9a0299aecbccc18e72fb495a64dfe2ffddd89931
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-12-09 16:15:12 -0800

    inital commit

---
+++ Basic-Datatypes/strings.rb
@@ -0,0 +1,17 @@
+
+# A String object in Ruby holds and manipulates a sequence of one or more characters.
+
+
+# This is a string
+"i love life"
+
+# Log the string to the console
+puts "What a great day it is"
+
+# This is how we store a string in a variable. Variables are a name referecne to a place in the computers memory
+greeting = "Hello how are you?"
+
+# Now log the variable to the console
+puts greeting
+
+# Concatenation is a way of joining two or more strings together to create a new string
